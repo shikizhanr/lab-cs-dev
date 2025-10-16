f0554c50b825bd6c952adcbc59156192d0ea572b
shikizhanr <zhanara.ausiilova@gmail.com>
2025-10-17 01:01:24 +0500

    add Liquibase

---
+++ ausiilova/src/main/resources/db/changelog/scripts/script1.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA utmn;
+
+CREATE TABLE utmn.student
+(
+    id bigserial NOT NULL,
+    fio varchar(128) NOT NULL
+);
+
+COMMENT ON TABLE utmn.student IS 'Таблица для хранения информации о студентах';
+
+COMMENT ON COLUMN utmn.student.fio IS 'ФИО';
