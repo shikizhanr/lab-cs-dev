0caf07cef19a5cf9f26ee8d70d83f8f2f1552779
shikizhanr <zhanara.ausiilova@gmail.com>
2025-10-16 23:21:05 +0500

    create project

---
+++ ausiilova/src/main/java/com/example/ausiilova/AusiilovaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ausiilova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AusiilovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AusiilovaApplication.class, args);
+	}
+
+}
---
+++ ausiilova/src/test/java/com/example/ausiilova/AusiilovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ausiilova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AusiilovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
